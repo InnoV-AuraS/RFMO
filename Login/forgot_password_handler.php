87af15cc5991de95badd92cc5894fde8fd008fe9
Snehasis-Das <snehasis779@gmail.com>
2025-01-31 17:11:24 +0530

    Final pages made

--- Login/forgot_password_handler.php
+++ Login/forgot_password_handler.php
@@ -1,3 +1,29 @@
 <?php
-echo "forgot_password_handler";
+function generateOTP(){
+    return rand(1000,9999);
+}
+function sendOTP(){}
+function verifyOTP($userotp,$otp){
+    return $otp==$userotp;
+}
+if(!isset($_SESSION["otp"])){
+    if(!isset($_POST["id"])){
+        echo "Session Expired";
+        exit;
+    }
+    $phone=$_POST["id"];
+    $otp=generateOTP();
+    $_SESSION["otp"]=$otp;
+    sendOTP($phone,$otp);
+}
+else{
+    $userotp=$_POST["otp"];
+    $otp=$_SESSION["otp"];
+    if(verifyOTP($userotp,$otp)){
+        session_unset();
+        session_destroy();
+        exit;
+    }
+    echo "INVALID OTP";
+}
 ?>
